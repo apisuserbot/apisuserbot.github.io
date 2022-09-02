61bae49d739a2d803e8f65705f3e19496d6c9bb6
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2022-09-02 12:57:39 +0700

    ...

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("google.chrome");
+
+<script>
+const person = {
+  firstName : "Apis",
+  lastName  : "Pis",
+  age     : 16,
+  eyeColor  : "blue"
+};
+
+document.getElementById("demo").innerHTML =
+person.firstName + " is " + person.age + " years old.";
+</script>
